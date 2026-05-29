50555497128aafedc4cfdc2e102ad8922152e638
Gaurav Rao <gauravraorao6@gmail.com>
2026-05-29 12:46:13 +0530

    prime number

---
+++ prime.java
@@ -0,0 +1,19 @@
+public class prime {
+        static boolean isprime(int num){
+                if (num<=0){
+                        return false;
+                }
+                 else{
+                  for (int i =2;i<num;i++) {
+                        if (num%i==0) {
+                return false;
+                                
+                        }
+                      
+                  }
+                 }return true;
+        }
+        public static void main(String[] args) {
+          System.out.println( isprime(20));     
+        }
+}
