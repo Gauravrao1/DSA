3e65af2f3fb43d6ae7070c349d8a5b2a8ee27cb3
Gaurav Rao <gauravraorao6@gmail.com>
2026-05-29 12:08:58 +0530

    palidrome_number

---
+++ palidrome_number.java
@@ -0,0 +1,28 @@
+public class palidrome_number {
+ static int reversenum(int num){
+        int revnum =0;
+        while (num!=0) {
+                int digit =num%10;
+                revnum=revnum*10+digit;
+                num =num/10;
+        }
+        return revnum;
+                
+        }
+ 
+        static boolean ispalidrome(int num){
+                int originalnum =num;
+                int reversednum = reversenum(num);
+                if (originalnum == reversednum) {
+                        System.out.println("it is palindrome");
+                        return true;
+                }else{
+                        System.out.println("it is not palindrome");
+                        return false;
+                }   
+               
+        }
+        public static void main(String[] args) {
+            System.out.println(ispalidrome(121));    
+        }
+}
