c9c0856099d060cdaaa8643d1b9cdecf45d92ff0
Gaurav Rao <gauravraorao6@gmail.com>
2026-05-30 10:28:44 +0530

    Lcm

---
+++ lcm.java
@@ -0,0 +1,24 @@
+public class lcm {
+        
+        static int getGcd(int a, int b){
+                //gcd(a,b)=gcd(b,a%b)
+                while (b!=0) {
+                        int temp =b;
+                        b = a%b;
+                        a =temp;
+                        
+                }
+                int ans =a;
+                return ans;
+        }
+        static int lcm(int a,int b){
+                int gcd =getGcd(a, b);
+                int prod =a*b;
+                int lcm =prod/gcd;
+                return lcm;
+        }
+
+        public static void main(String[] args) {
+                System.out.println(lcm(10, 20));
+        }
+}
