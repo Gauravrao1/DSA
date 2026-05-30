896395a48c2f628561950ce834ff15dfa6df9e55
Gaurav Rao <gauravraorao6@gmail.com>
2026-05-30 10:21:16 +0530

    Gcd

---
+++ Gcd.java
@@ -0,0 +1,17 @@
+public class Gcd {
+        static int getGcd(int a, int b){
+                //gcd(a,b)=gcd(b,a%b)
+                while (b!=0) {
+                        int temp =b;
+                        b = a%b;
+                        a =temp;
+                        
+                }
+                int ans =a;
+                return ans;
+        }
+
+        public static void main(String[] args) {
+                System.out.println(getGcd(18, 12));
+        }
+}
