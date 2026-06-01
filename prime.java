115c109f3a680c63859a1dbaba9ce648455c3fad
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-01 23:29:55 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -11,7 +11,7 @@ public class prime {
                        }
                  }
                   return true;
-                 }
+                 } 
         public static void main(String[] args) {
           System.out.println( isprime(20));     
         }
