f62e2748e903edb6f01eef651a7782d5fbc6c196
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-01 23:16:47 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -7,7 +7,7 @@ public class prime {
                   for (int i =2;i<num;i++) {
                         if (num%i==0) {
                            return false;
-                                 }
+                                  }
                        }
                  }
                   return true;
