ea67eafca441f64503f9daed745bd43e096c4ea1
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-01 23:18:13 +0530

    Update Armstrong.java
    Fix indentation and remove unnecessary else block.

--- Armstrong.java
+++ Armstrong.java
@@ -12,8 +12,7 @@ public class Armstrong {
                 return true;
             }else{
                 return false;
-            }
-                
+            }    
             }
                public static void main(String[] args) {
                System.out.println(isAmstrongnumber(153));
