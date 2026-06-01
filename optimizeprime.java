23c36c71a6c010d76dfd082ff5927a51c14d47ae
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-01 23:28:55 +0530

    Update optimizeprime.java

--- optimizeprime.java
+++ optimizeprime.java
@@ -7,7 +7,7 @@ public class optimizeprime {
                 if(num%i == 0){
                 return false;
                 }
-              }return true;
+              }return true; 
         }
         public static void main(String[] args) {
                 System.out.println(isprime(4)); 
