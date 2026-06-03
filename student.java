5aea3ada36cbe578ea390ae543621e1ecec4e64f
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-03 23:16:28 +0530

    object concepts

---
+++ student.java
@@ -0,0 +1,18 @@
+public class student{
+        public int id;
+        public int age;
+        public String name;
+        public int nos;
+        public student(){
+                System.out.println("Student Default   ctor called ");
+        }
+        public void study(){
+                System.out.println(name+"Studying");
+        }
+        public void sleep(){
+                System.out.println(name+"sleeping");
+        }
+        public void bunk(){
+         System.out.println(name+"Bunking");
+        }
+}
