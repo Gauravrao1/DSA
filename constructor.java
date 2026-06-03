8c0fb334850b6c9da41d97665d1ce998a045a57f
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-03 23:34:45 +0530

    constructor

--- constructor.java
+++ constructor.java
@@ -1,3 +1,18 @@
 public class constructor {
-        
+        public static void main(String[] args) throws Exception {
+                student A =new student();
+                A.id =1;
+                A.age = 14;
+                A.name ="Ranu";
+                A.nos =5;
+                System.out.println(A.name);
+                System.out.println(A.age);
+                System.out.println(A.id);
+                System.out.println(A.nos);
+
+                A.bunk();
+                A.study();
+                A.sleep();
+
+        }
 }
