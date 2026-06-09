8c68633f102c45b8061a80954fc90c5093ff3747
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-09 22:38:08 +0530

    fabonacci

---
+++ fabonacci.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class fabonacci {
+        static int fabonaccinum(int n){
+                if(n ==0) return 0;
+                if(n==1) return 1;
+                return fabonaccinum(n-1)+fabonaccinum(n-2);
+        }
+        public static void main(String[] args) {
+                Scanner sc =new Scanner(System.in);
+                int n = sc.nextInt();
+            System.out.println(fabonaccinum(n));
+        }
+}
