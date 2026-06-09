e9d198bdb8f0b0f4358954c7bc80f2e5b04ab7f7
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-09 21:23:12 +0530

    natural decesing

--- naturalnumberdecresing.java
+++ naturalnumberdecresing.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class naturalnumberdecresing {
-        
+        static void printdecrsingnatural(int n){
+                if ( n==1) {
+                   System.out.println(1);
+                    return;    
+                }
+                System.out.println(n);
+           printdecrsingnatural(n-1);    
+        }
+        public static void main(String[] args) {
+                Scanner sc =new Scanner(System.in);
+                int n = sc.nextInt();
+                printdecrsingnatural(n);
+        }
 }
