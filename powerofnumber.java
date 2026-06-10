f915cc00ce09cd7d34a191eb80c8c6bda799aac1
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-10 22:17:00 +0530

    using q/2

--- powerofnumber.java
+++ powerofnumber.java
@@ -1,8 +1,15 @@
 //Given Two number p&q Find value p^q using recirsive function
 public class powerofnumber {
+        // static int powerofnum(int p,int q){
+        //         if (q==0) return 1;
+        //        return p*powerofnum(p,q-1); 
+        // }
         static int powerofnum(int p,int q){
-                if (q==0) return 1;
-               return p*powerofnum(p,q-1); 
+ if (q==0) return 1; 
+ int smallpow = powerofnum(p, q/2); 
+ if (q%2==0) {
+         return smallpow*smallpow;       
+  }return p*smallpow*smallpow;  
         }
         public static void main(String[] args) {
                 int p=20;
