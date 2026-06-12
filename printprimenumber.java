c2a791b040d6cd633f54656eec712788c67c4ad8
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-12 22:11:58 +0530

    Update printprimenumber.java

--- printprimenumber.java
+++ printprimenumber.java
@@ -7,7 +7,7 @@ public class printprimenumber {
          for(int var1 = 2; var1 < var0; ++var1) {
             if (var0 % var1 == 0) {
                return false;  
-            }
+            } 
          }
 
          return tru e;
