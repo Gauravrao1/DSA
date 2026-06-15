45d4d0ddd917090681a0f0e5637c5ab946551549
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-15 22:42:22 +0530

    Update countdigit.java

--- countdigit.java
+++ countdigit.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class countdigit {
 static int countdigitnum(int n){
     if(n <= 9) 
-        return 1;
+        return 1; 
 
     return 1 + countdigitnum(n / 10);
 }
