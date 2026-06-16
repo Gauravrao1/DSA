26933e28fd832d718b2d32aef8534dae40fb1b09
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-16 22:15:55 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -3,7 +3,7 @@ public class constructor {
                 student A =new student();
                 A.id =1;
                 A.age = 14;
-                A.name ="Ranu";
+                A.name ="Ranu"; 
                 A.nos =5;
                 System.out.println(A.name);
                 System.out.println(A.age);
