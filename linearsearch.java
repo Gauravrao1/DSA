d9e0aa2f6aea9608c03800a14da36f6989a8dc1f
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-17 20:57:14 +0530

    linearsearch

---
+++ linearsearch.java
@@ -0,0 +1,17 @@
+public class linearsearch {
+        static boolean linearsearchinarr(int[]arr,int idx,int target){
+               if (idx==arr.length) return false;
+                if (arr[idx] ==target)     return true;
+                return linearsearchinarr(arr,idx+1,target);
+        }
+        public static void main(String[] args) {
+                int arr []={10,20,15,45,23};
+                int target=15;
+               if (linearsearchinarr(arr,0,target)) {//==will known result true
+                System.out.println("yes");
+                
+               } else{
+                System.out.println("No");
+               }
+        }
+}
--- sumofarray.java
+++ sumofarray.java
@@ -13,3 +13,4 @@ public class sumofarray {
                 System.out.println("Sum ="+ result);
         }
 }
+   
