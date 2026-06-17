d3502558e507989558412f1176a2c7386f008c14
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-17 09:43:06 +0530

    arraysum

---
+++ sumofarray.java
@@ -0,0 +1,15 @@
+//Find the sum of the element of the array [2,3,5,20,1]
+import java.util.*;
+public class sumofarray {
+        static int sumofarray(int arr[],int idx){
+                 if (idx ==arr.length-1) {
+                return arr[idx];
+                }
+               return arr[idx]+sumofarray(arr, idx+1);
+        }
+        public static void main(String[] args) {
+                int arr []={2,3,5,20,1};
+                int result = sumofarray(arr,0);
+                System.out.println("Sum ="+ result);
+        }
+}
