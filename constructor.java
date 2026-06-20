36525d6dfe038caf3ff66a4116214e4b9b7e8a69
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-20 21:37:59 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -8,7 +8,7 @@ public class constructor {
                 System.out.println(A.name);
                 System.out.println(A.age);
                 System.out.println(A.id);
-                System.out.println(A.nos);
+                System.out.println(A.nos); 
 
                 A.bunk();
                 A.study();
