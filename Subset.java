3fdeffc769f8ce6c2fa90663ab8fe7e2def76c68
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-23 17:55:57 +0530

    adding question

--- Subset.java
+++ Subset.java
@@ -1,3 +1,9 @@
+/*Given an array of integers, print sums of all subsets in it. Output sums can be printed in any order.
+Input: arr[] = {2,3}
+Output: 0235
+Input: arr[] = {2, 4, 5}
+Output: 0245679 11
+ */
 public class Subset {
         static  void subarrayprintsum(int []a,int n,int idx ,int currsum){
                 if (idx >=n) {
