eedacbaa4b0dde362d889842f38db3a5beaa58aa
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-25 10:54:02 +0530

    selection sort

--- selectionsort.java
+++ selectionsort.java
@@ -1,3 +1,26 @@
 public class selectionsort {
-        
+        static void selectionsortarr(int[]arr){
+                int n =arr.length;
+                for(int i=0;i<n-1;i++){
+                 int min_index =i;
+                 for(int j=i+1;j<n;j++){
+                        if(arr[j]<arr[min_index]){
+                                min_index=j;
+                        }
+                 }
+                 int temp =arr[i];
+                 arr[i]=arr[min_index];
+                 arr[min_index]=temp;
+
+                 
+                 
+                }
+        }
+      public static void main(String[] args) {
+        int []arr={7,4,5,1,2};
+        selectionsortarr(arr);
+        for (int i : arr) {
+                System.out.print(i+" ");
+        }
+      }  
 }
