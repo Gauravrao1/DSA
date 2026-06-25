ccd96f0df03c35ad04a069f600f6000aad71e1d4
Gaurav Rao <gauravraorao6@gmail.com>
2026-06-25 09:03:19 +0530

    bubble sort

---
+++ bubblesort.java
@@ -0,0 +1,20 @@
+public class bubblesort {
+        static void sorting(int a[]){
+                for(int i=0;i<a.length-1;i++){
+                        for(int j =0;j<a.length-i-1;j++){
+                    if (a[j]>a[j+1]) {
+                                 int temp=a[j];
+                                  a[j]=a[j+1];
+                                 a[j+1]=temp;
+                                }
+                        }
+                }
+        }
+      public static void main(String[] args) {
+        int a []={30,20,50,58,90};
+       sorting(a);
+        for (int  i : a) {
+              System.out.print(i+" ");  
+        }
+      }  
+}
