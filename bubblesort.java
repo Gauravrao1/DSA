e46ccb808e9435c7d377a27e9f267c2fa5698765
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-06-29 20:38:19 +0530

    Update bubblesort.java

--- bubblesort.java
+++ bubblesort.java
@@ -6,7 +6,7 @@ public class bubblesort {
                                  int temp=a[j];
                                   a[j]=a[j+1];
                                  a[j+1]=temp;
-                                }
+                                } 
                         }
                 }
         }
