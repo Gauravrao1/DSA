b6f5ffb9473f348c3524dd90cdf845d95c850dc2
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-07-06 22:47:22 +0530

    Update Armstrong.java

--- Armstrong.java
+++ Armstrong.java
@@ -2,7 +2,7 @@ public class Armstrong {
         static boolean isAmstrongnumber(int num){
                 int sum =0;
                 int original =num;
-                while (num!=0) {
+                while (num!=0) { 
                 int digit =num%10;
                 int cubeofDigit =digit*digit*digit;
                 sum =sum+cubeofDigit;
