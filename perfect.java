d0e260d535980c181d00551e523967579e30043b
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-07-15 23:02:20 +0530

    Update perfect.java

--- perfect.java
+++ perfect.java
@@ -2,7 +2,7 @@ public class perfect {
         static boolean checkperfectnumber(int num){
                 int sum =1;
                 for(int i=2;i*i<=num;i++){
-
+ 
                 if (num%i ==0){
                         int firstFactor =i;
                         int secondfactor =num/i;
