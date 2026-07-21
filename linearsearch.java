74e0bf5411dadb8ba888c3b2be1ad09b4a7eb95d
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-07-21 23:20:36 +0530

    Update linearsearch.java

--- linearsearch.java
+++ linearsearch.java
@@ -4,7 +4,7 @@ public class linearsearch {
                 if (arr[idx] ==target)     return true;
                 return linearsearchinarr(arr,idx+1,target);
         }
-        public static void main(String[] args) { 
+        public static void main(String[] args) {  
                 int arr []={10,20,15,45,23};
                 int target=15;
                if (linearsearchinarr(arr,0,target)) {//==will known result true
