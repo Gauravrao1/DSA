b7744411d504336ae0c9aa13b019cace438f2eaa
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-07-25 23:35:12 +0530

    Update Subset.java

--- Subset.java
+++ Subset.java
@@ -6,7 +6,7 @@ Output: 0245679 11
  */
 public class Subset {  
         static  void subarrayprintsum(int []a,int n,int idx ,int currsum){
-                if (idx >=n) {
+                if (idx >=n) { 
                  System.out.println(currsum); 
                  return;
                 }
