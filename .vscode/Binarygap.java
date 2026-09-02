da75630ca1823b0748a853efb375e156b686ebbb
Gaurav Rao <163044518+Gauravrao1@users.noreply.github.com>
2026-09-02 22:53:34 +0530

    Create Binarygap.java

---
+++ .vscode/Binarygap.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int binaryGap(int n) {
+
+        int prev = -1;
+        int ans = 0;
+        int index = 0;
+
+        while (n > 0) {
+
+            if ((n & 1) == 1) {
+
+                if (prev != -1) {
+                    ans = Math.max(ans, index - prev);
+                }
+
+                prev = index;
+            }
+
+            n = n >> 1;
+            index++;
+        }
+
+        return ans;
+    }
+}
