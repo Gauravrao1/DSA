93ed80c9fadc0de6d242cfe5e2c7817bd70eae35
Gaurav Rao <gauravraorao6@gmail.com>
2026-09-04 23:12:03 +0530

    reverse

---
+++ stringrev.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class stringrev {
+        public static void main(String[] args) {
+                
+        
+      StringBuilder sb =new StringBuilder("Physics");
+      sb.reverse(); 
+      System.out.println(sb); 
+}
+}
