7a01d41a8b81253626fd7facecc56b84a27f4dae
Gaurav Rao <gauravraorao6@gmail.com>
2026-09-09 19:40:07 +0530

    reversesentencce

---
+++ reversewordInSentence.java
@@ -0,0 +1,20 @@
+public class reversewordInSentence{
+        public static void main(String[] args) {
+             String str ="I am an online educator";   
+             String ans =" ";
+             StringBuilder sb =new StringBuilder(" ");
+             for(int i=0;i<str.length();i++){
+                char ch =str.charAt(i);
+                if (ch!=' ') {
+                        sb.append(ch);
+                            }
+             else{
+                sb.reverse();
+                ans += sb;
+                ans +=" ";
+                sb.delete(0, sb.length());//sb =new StringBuilder("");
+             }
+        } sb.reverse();
+        ans+=sb;
+        System.out.println(ans);
+}}
