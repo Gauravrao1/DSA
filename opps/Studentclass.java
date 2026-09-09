d6fe4f6e568db43f341c39afbc55a0351df5cae4
Gaurav Rao <gauravraorao6@gmail.com>
2026-09-09 20:22:24 +0530

    stringclass

---
+++ opps/Studentclass.java
@@ -0,0 +1,14 @@
+public class Studentclass { 
+    public static class Student { 
+        String name; 
+        int rno; 
+        double percent; 
+    } 
+ 
+    public static void main(String[] args) { 
+        Student x = new Student(); 
+        x.name = "Gaurav"; 
+        x.percent = 95.6; 
+        System.out.println(x.name); 
+    } 
+}
