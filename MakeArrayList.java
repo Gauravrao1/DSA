b2d3f02e95a3d6a8a6135770ab70bdd379ed2c7b
Gaurav Rao <gauravraorao6@gmail.com>
2026-09-13 13:08:37 +0530

    makeArrayList

---
+++ MakeArrayList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class MakeArrayList {
+     public static void main(String[] args) {
+        ArrayList<Integer>arr = new ArrayList<>(4);
+        arr.add(1);
+        arr.add(2);
+        arr.add(3);
+        arr.add(5);
+        arr.add(6);
+        System.out.println(arr.size());
+        System.out.println(arr);
+     }   
+}
