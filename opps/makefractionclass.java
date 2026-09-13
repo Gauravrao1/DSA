01bc6a7b5c5a1d7ac5b47c083796b5b780067c05
Gaurav Rao <gauravraorao6@gmail.com>
2026-09-13 12:50:06 +0530

    Simplify fraction

--- opps/makefractionclass.java
+++ opps/makefractionclass.java
@@ -1,16 +1,29 @@
-public class makefractionclass {
-public static  class fraction{
+public class Fraction {
+        public static int gcd (int num ,int den){
+                int min =Math.min(num,den);
+                for(int i=min;i>=1;i--){
+                        if (num%i==0&&den%i==0) return i; 
+                                
+                        }return  min;
+                }
+        
+
         int num;
         int den;
         public fraction(int num,int den){
                 this.num =num;
                 this.den =den;
         }
+        public void simplify(){
+        int hcf =gcd(num,den);
+        num /=hcf;
+        den /=hcf;
+        }
 
-}
         public static void main(String[] args) {
-        fraction f1 =new fraction(3,7);
+        Fraction f1 =new Fraction(3,7);
         System.out.println(f1.num+"/"+f1.den);
-     
-    }    
+        f1.simplify();
+        System.out.println(f1.num+"/"+f1.den);
+    }
 }
