ba0582d1a4747b6da16daaf9f22bd6168d995451
Gaurav Rao <gauravraorao6@gmail.com>
2026-09-13 12:34:27 +0530

    fraction oops

---
+++ opps/makefractionclass.java
@@ -0,0 +1,16 @@
+public class makefractionclass {
+public static  class fraction{
+        int num;
+        int den;
+        public fraction(int num,int den){
+                this.num =num;
+                this.den =den;
+        }
+
+}
+        public static void main(String[] args) {
+        fraction f1 =new fraction(3,7);
+        System.out.println(f1.num+"/"+f1.den);
+     
+    }    
+}
